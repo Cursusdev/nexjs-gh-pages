79eb6412d0c851968b3d0015b5237aa2e4beab89
httpcode <yannick806@gmail.com>
2021-03-13 17:58:55 +0100

    fix 5_github actions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const isProd = (process.env.NODE_ENV || 'production') === 'production'
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : '',
+    NEXT_PUBLIC_BASE_PATH: isProd ? '/nexjs-gh-pages' : '',
   },
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
